54ec5d1740f5467d6a92530088c2a113977b6c67
Aaditya Brahmbhatt <aadityabrahmbhatt@gmail.com>
2021-05-30 17:58:34 +0530

    Fix firebase app initialization

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+var apiKey = "AIzaSyBHGPPUYCC5_UwkdqX4bB9dPdqzCufYN54";
+var authDomain = "cowar-github.firebaseapp.com";
+var projectId = "cowar-github";
+var storageBucket = "cowar-github.appspot.com";
+var messagingSenderId = "710448594827";
+var appId = "1:710448594827:web:2b213f4de723eeff04b98e";
+var measurementId = "G-XNTYSR7J0X";
+
+export var firebaseConfig = {
+                 apiKey: apiKey,
+                 authDomain: authDomain,
+                 projectId: projectId,
+                 storageBucket: storageBucket,
+                 messagingSenderId: messagingSenderId,
+                 appId: appId,
+                 measurementId: measurementId
+               };
